b02ae0e1e5bf141c14a0f7c4902418b87ba3b604
KelOkekpe <joseph@callrail.com>
2021-05-02 19:35:52 -0400

    first commit

---
+++ test/pageobjects/tictactoe.page.js
@@ -0,0 +1,102 @@
+const Page = require('./page');
+
+
+class TicTacToePage extends Page {
+
+    open () { return super.open('https://codepen.io/CalendlyQA/full/KKPQLmV'); }
+
+    // selector for number input text box
+    get inputNumber () { 
+        return $('#number'); 
+    }
+
+    // selector for play button
+    get btnSubmit () { 
+        return $('#start');
+     }
+
+     // selector for tic tac toe board
+     get gameBoard ()  {
+        return $('//*/table');
+        //  return $('#table');     
+     }
+
+     get boardCells () {
+        return $$('td');
+     }
+     
+     // selector for message displayed when someone has won
+     get endGameMessage () {
+         return $('#endgame');
+     }
+    
+    // returns innerText of complete row of cells in string format, (i.e, X X X)
+    rowText (index) {
+        var table = $('//*/table')
+        var rows = table.$$(".//tr")
+        var rowText = rows[index].getText()
+        return rowText;
+      }
+
+      // returns innerText of complete column of cells in string format, (i.e, X X X)
+      columnText (rowLength, column) {
+        var str = ""
+        var reference = column
+        for(var i = 0;  i < rowLength -  1; i++)  {
+            if(i==0){
+                var str2 = `${$$('td')[column].getText()} `
+                str = str.concat (str2)
+            }
+            str2 = `${$$('td')[reference + rowLength].getText()} `
+            str = str.concat (str2)
+            reference += rowLength
+        }
+        return str.replace(/\s+$/, "");
+      }
+
+    // intended use is to return the innerText of a combo of cells in a diagonal in string format
+    // example: if you wanted to check the values of diagonal cells from top left to the bottom right
+    // you would input the cell indices and this function will return them in "X X X" or "O O O" format
+    // note: flexible enough to return any combo of cells in any formation (rows, columns or random)
+    diagonalText (...args) {
+        var table = $('//*/table');
+        var cells = table.$$(".//tr/td")
+        var str = ""
+        var str2 = "X "
+
+        for(var i=0;i<args.length;i++){
+            args.forEach(arg => 
+                str2 = `${cells[arg].getText()} `,
+                str = str.concat (str2))
+        }
+        return str.replace(/\s+$/, "");
+    }
+
+    // tic tac toe game form is nested inside iframe
+    // we'll have to use switchToFrame to interact with elements within the iframe
+    switchToFrame () {
+        browser.switchToFrame($('#result'));
+    }
+    
+    // this creates a board, cellCount determines the amount of cells the board contains
+    createBoard (cellCount) {
+        this.inputNumber.setValue(cellCount);
+        this.btnSubmit.click();
+    }
+    
+    // returns a board cell by  index
+    boardCell (index)  {
+        return $$('td')[index];
+    }
+
+    // selects multiple board cells
+    // remember: symbols alternate every subsequent index, (i.e, [0,1,2] =  X O X)
+    selectBoardCells (args) {
+       for(var i=0; i<args.length; i++){
+        $$('td')[args[i]].click();
+       }      
+    }
+}
+
+module.exports = new TicTacToePage();  
+
---
+++ test/specs/test-tictactoe.js
@@ -0,0 +1,107 @@
+const TicTacToePage = require('../pageobjects/tictactoe.page');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 400000;
+
+
+describe('Tic Tac Toe Game', function () {
+
+    beforeEach(function () {
+        require('expect-webdriverio').setOptions({ wait: 5000 })
+        TicTacToePage.open(); 
+        TicTacToePage.switchToFrame();
+    });
+
+    function playerXWins() {
+        return "Congratulations player X! You've won. Refresh to play again!"
+    }
+
+    //generates a string of X's ; the number of X's are equal to the value paassed to this function
+    //this function allows flexibility when testing game results for boards w/ differing sizes
+    function dynamicRowOfXs(boardSize){
+        var str = ""
+        for(i=0; i<boardSize; i++ ){
+            str2 = "X "
+            str = str.concat (str2)
+        }
+        return str.replace(/\s+$/, "");
+    }
+
+    // returns the total number of cells containing an X or O 
+    function numberOfBoardEntries() {
+        var arr = []
+        for(i=0;i<$$('td').length;i++){
+            if($$('td')[i].getText() == "O" || $$('td')[i].getText() == "X"){
+                arr.push($$('td')[i].getText())
+            }
+        }
+        return arr.length;
+    }
+
+    // it('creates a 3 by 3 board', function () {
+    //     TicTacToePage.createBoard('3');
+    //     expect(TicTacToePage.gameBoard).toBeExisting()
+    // });
+
+    // it('alternates user symbols after every turn', function () {
+    //     var cellsPickedArray =  [0,3]
+    //     TicTacToePage.createBoard('3');
+    //     TicTacToePage.selectBoardCells(cellsPickedArray);
+    //     expect(TicTacToePage.boardCell(0)).toHaveText('X')
+    //     expect(TicTacToePage.boardCell(3)).toHaveText('O')      
+    // });
+
+    it('declares tie game for games with no winner', function (){
+        var cellsPickedArray = [0,1,4,8,5,3,2,6,7]
+        TicTacToePage.createBoard('3');
+        TicTacToePage.selectBoardCells(cellsPickedArray);
+        
+        //checks if boaard is full
+        if(numberOfBoardEntries() == TicTacToePage.boardCells.length){
+            expect(TicTacToePage.endGameMessage).toBeDisplayed()
+        }
+    });
+
+    
+    it('declares correct game winner by horizontal victory', function () {
+        var cellsPickedArray =  [0,3,1,4,2]
+        TicTacToePage.createBoard('3');
+        TicTacToePage.selectBoardCells(cellsPickedArray);
+
+        if(TicTacToePage.rowText(0) == dynamicRowOfXs(3) ){
+            expect(TicTacToePage.endGameMessage).toHaveText(playerXWins())
+        }
+    });
+ 
+    it('declares correct game winner by vertical victory', function () {
+        var cellsPickedArray =  [0,1,3,2,6]
+        TicTacToePage.createBoard('3');
+        TicTacToePage.selectBoardCells(cellsPickedArray);
+
+        if(TicTacToePage.columnText(3,0) == dynamicRowOfXs(3) ){
+            expect(TicTacToePage.endGameMessage).toHaveText(playerXWins())
+        }
+    });
+
+    it('declares correct game winner by upper left corner to bottom right corner victory', function () {
+        var cellsPickedArray =  [0,1,6,2,12,4,18,9,24]
+        TicTacToePage.createBoard('5');
+        TicTacToePage.selectBoardCells(cellsPickedArray);
+        
+        if(TicTacToePage.diagonalText(0,6,12,18,24) == dynamicRowOfXs(5)){
+            expect(TicTacToePage.endGameMessage).toHaveText(playerXWins())
+        }
+    });
+
+    it('declares correct game winner by upper right corner to bottom left corner victory', function () {
+        var cellsPickedArray =  [2,1,4,3,6]
+        TicTacToePage.createBoard('3');
+        TicTacToePage.selectBoardCells(cellsPickedArray);
+
+        if(TicTacToePage.diagonalText(2,4,6) == dynamicRowOfXs(3)){
+            expect(TicTacToePage.endGameMessage).toHaveText(playerXWins())       
+        }
+    });
+    
+});
+
+ 
